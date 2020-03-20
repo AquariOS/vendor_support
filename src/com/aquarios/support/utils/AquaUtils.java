f0856a86ffcf49a4a612d58cb02f576e3dd6a9b4
Alex <du.alexcruz@gmail.com>
2020-03-20 21:39:28 +0700

    AquaUtils: Add method for finding temp and farhenheit/celsius
    Change-Id: Iaf0bd2c509283e296a5c18c0ab2efb4b89f16c39

--- src/com/aquarios/support/utils/AquaUtils.java
+++ src/com/aquarios/support/utils/AquaUtils.java
@@ -25,6 +25,7 @@ import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -37,7 +38,7 @@ import android.hardware.input.InputManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.UserHandle;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
@@ -45,8 +46,10 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.MediaStore;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
@@ -57,6 +60,7 @@ import android.view.IWindowManager;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 import com.android.internal.R;
@@ -449,5 +453,19 @@ public class AquaUtils {
                 // do nothing.
             }
         }
+    }
+
+    // Battery temperature utility
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round to nearest integer
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 }
