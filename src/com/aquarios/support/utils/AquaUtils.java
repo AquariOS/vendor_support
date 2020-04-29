f069065daf1af8a03a4128eec11f95d487e01015
maxwen <max.weninger@gmail.com>
2020-04-29 07:50:15 -0400

    AquaUtils: Add method for checking for package availability
    Change-Id: I0a45e6df1b230d269380d189d24ecc330f9349c1

--- src/com/aquarios/support/utils/AquaUtils.java
+++ src/com/aquarios/support/utils/AquaUtils.java
@@ -468,4 +468,17 @@ public class AquaUtils {
         return String.valueOf((n - c) % 2 == 0 ? (int) temp :
                 ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
+
+    public static boolean isPackageAvailable(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
 }
