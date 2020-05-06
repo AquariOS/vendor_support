5924d80df689e2bfb8cd6bcaddac2e59f03cb38a
calebcabob <calphonic@gmail.com>
2020-05-06 12:43:02 -0400

    AquaUtils: Create some custom device groups to check against
    Not all devices support the same custom features. A bool
    is typically needed in order to hide/show them correctly.
    With this, we can hopefully make it a little easier to
    conditionalize these things without the need to create
    extra bools/configs to the supported/unsupported devices.
    
    This just uses a pre-existing AOSP method and checks for
    the device name in your current build. This is very easy
    to use and modify. Using SystemProperties mostly worked
    initially, but there were some flukes. This way is just
    more dependable.
    
    Change-Id: I789ac178cbbfb72eac8aff021c8615a2bca6c61e

--- src/com/aquarios/support/utils/AquaUtils.java
+++ src/com/aquarios/support/utils/AquaUtils.java
@@ -481,4 +481,16 @@ public class AquaUtils {
             return false;
         }
     }
+
+    public static boolean isOlderPixelDevice() {
+		String deviceName = android.os.Build.DEVICE;
+		return ("crosshatch".equals(deviceName) || "blueline".equals(deviceName)
+		        || "taimen".equals(deviceName) || "walleye".equals(deviceName)
+		        || "bonito".equals(deviceName) || "sargo".equals(deviceName));
+    }
+
+    public static boolean isNewerPixelDevice() {
+		String deviceName = android.os.Build.DEVICE;
+		return ("coral".equals(deviceName) || "flame".equals(deviceName));
+    }
 }
